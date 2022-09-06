1b05f559f6bd8601fbda06529ac8459b9cbdf7ae
Guillaume BILLEY <guillaume.billey@pharmagest.com>
2022-09-06 14:45:38 +0200

    Fix postgrest access

--- front/src/dataProvider/rest.ts
+++ front/src/dataProvider/rest.ts
@@ -1,5 +1,15 @@
-import simpleRestProvider from "ra-data-simple-rest";
+import { fetchUtils, Options } from "react-admin";
+import postgrestRestProvider from "@promitheus/ra-data-postgrest";
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoid2ViX2Fub24ifQ.hvke4pCbLXzfEX1DMTroQ-g9TK5no_yN3OfQJuSjBT0";
+const httpClient = (url: string, options: Options = {}) => {
+  options.user = {
+    authenticated: true,
+    token: `Bearer ${TOKEN}`,
+  };
+  return fetchUtils.fetchJson(url, options);
+};
 
-const restProvider = simpleRestProvider("http://api:8000");
+const restProvider = postgrestRestProvider("http://localhost:8000", httpClient);
 
 export default restProvider;
